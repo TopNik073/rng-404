1c7f535b209138f2fad9be77b85b00b2076b88b1
Nikita Borzov <borzovn169@gmail.com>
2025-10-22 01:31:00 +0200

    Fix env vars on remote server

--- src/presentation/api/v1/rng/router.py
+++ src/presentation/api/v1/rng/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, UploadFile, File
 from src.presentation.api.v1.rng.dep import RNG_SERVICE_DEP
 from src.presentation.api.v1.rng.models import GEN_REQ_SCHEMA
 
-rng_router = APIRouter(prefix='/rng')
+rng_router = APIRouter(prefix='/rng', tags=['RNG'])
 
 @rng_router.get('/generate')
 async def rng_generator(
