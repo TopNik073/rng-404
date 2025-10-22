d632494bdca1a0eee651394241cc8a3afd96fa1b
Nikita Borzov <borzovn169@gmail.com>
2025-10-23 01:33:41 +0200

    Feat endpoint for binary files

--- src/presentation/api/v1/rng/router.py
+++ src/presentation/api/v1/rng/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Query
 from fastapi.responses import StreamingResponse, Response
 
 from src.presentation.api.v1.rng.dep import RNG_SERVICE_DEP
@@ -14,3 +14,10 @@ async def rng_generator(
     file: UploadFile | None = File(default=None),  # noqa
 ) -> GeneratorResponseSchema | StreamingResponse | Response:
     return await rng_service.generate(params=params, upload_file=file)
+
+@rng_router.get('/generate/binary-file')
+async def rng_generator_binary_file(
+    rng_service: RNG_SERVICE_DEP,
+    length: int = Query(ge=0, default=1_000_000),
+) -> StreamingResponse:
+    return await rng_service.generate_binary_file(length=length)
--- src/services/rng/service.py
+++ src/services/rng/service.py
@@ -20,9 +20,9 @@ class RngService:
 
     async def generate(
         self, params: GenerateRequestSchema, upload_file: UploadFile | None
-    ) -> GeneratorResponseSchema | StreamingResponse | Response:
+    ) -> GeneratorResponseSchema | Response:
         if upload_file and not upload_file.filename.endswith('.mp3'):
-            raise HTTPException(400, 'Invalid file extension')
+            raise HTTPException(400, 'Invalid file extension, mp3 expected')
 
         if upload_file and upload_file.filename.endswith('.mp3'):
             with tempfile.NamedTemporaryFile(suffix='.mp3') as tmp:
@@ -31,48 +31,59 @@ class RngService:
                 audio = MP3(tmp.name)
                 duration = audio.info.length
 
+            # We can't proceed big files
             if duration >= env_config.MAX_AUDIO_DURATION:
-                raise HTTPException(400, 'Audio duration is too long.')
+                raise HTTPException(400, f'Audio duration is too long. Should be less than {env_config.MAX_AUDIO_DURATION} seconds')
 
             upload_file.file.seek(0)
 
         random_result: list[str] = await self.rng.get_random(params, upload_file=upload_file)
 
-        if params.format == 'txt':
-            content = '\n'.join(random_result)
-            file_like = BytesIO(content.encode('utf-8'))
-            return StreamingResponse(
-                file_like,
-                media_type='text/plain',
-                headers={'Content-Disposition': 'attachment; filename="random.txt"'},
-            )
+        msg = MIMEMultipart('mixed')
 
-        if self.rng.executed_images:
-            msg = MIMEMultipart('mixed')
+        # Attach json data
+        json_payload = {
+            "executed_sources": [src.model_dump() for src in self.rng.executed_sources],
+            "seed": self.rng.bytes_to_bits(self.rng.seed),
+            "result": random_result,
+        }
+        json_part = MIMEApplication(json.dumps(json_payload), "json")
+        json_part.add_header("Content-Disposition", "attachment", filename="result.json")
+        msg.attach(json_part)
 
-            # JSON часть
-            json_part = MIMEApplication(
-                json.dumps(
-                    {
-                        'executed_sources': [source.model_dump() for source in self.rng.executed_sources],
-                        'seed': self.rng.bytes_to_bits(self.rng.seed),
-                        'result': random_result,
-                    }
-                ),
-                'json',
-            )
-            json_part.add_header('Content-Disposition', 'attachment', filename='result.json')
-            msg.attach(json_part)
+        # Attach txt if needed
+        if params.format == "txt":
+            txt_part = MIMEApplication("\n".join(random_result).encode("utf-8"), "txt")
+            txt_part.add_header("Content-Disposition", "attachment", filename="random.txt")
+            msg.attach(txt_part)
 
-            for i, img_buf in enumerate(self.rng.executed_images, start=1):
-                img_part = MIMEApplication(img_buf.getvalue(), 'png')
-                img_part.add_header('Content-Disposition', 'attachment', filename=f'plot_{i}.png')
-                msg.attach(img_part)
+        # Attach graphics
+        for i, img_buf in enumerate(self.rng.executed_images, start=1):
+            img_part = MIMEApplication(img_buf.getvalue(), "png")
+            img_part.add_header("Content-Disposition", "attachment", filename=f"plot_{i}.png")
+            msg.attach(img_part)
 
-            return Response(content=msg.as_bytes(), media_type=f'multipart/mixed; boundary={msg.get_boundary()}')
+        return Response(
+            content=msg.as_bytes(),
+            media_type=f'multipart/mixed; boundary="{msg.get_boundary()}"',
+        )
 
-        return GeneratorResponseSchema(
-            executed_sources=self.rng.executed_sources,
-            seed=self.rng.bytes_to_bits(self.rng.seed),
-            result=random_result,
+    async def generate_binary_file(self, length: int) -> StreamingResponse:
+        random_result: list[str] = await self.rng.get_random(
+            params=GenerateRequestSchema(
+                from_num='0',
+                to_num='1',
+                count=length,
+                base=2,
+                uniq_only=False,
+                format='txt',
+            ),
+            upload_file=None
+        )
+        return StreamingResponse(
+            content=BytesIO(
+                ''.join(random_result).encode('utf-8'),
+            ),
+            media_type='text/plain',
+            headers={'Content-Disposition': 'attachment; filename="random.txt"'},
         )
